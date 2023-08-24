bc48775f84f53b21ffccc53b1fa215b337173308
pizzosta <pizzostor@gmail.com>
2023-08-24 15:20:26 +0000

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+
 def log_stats(logs_collection, option=None):
     """
     Get statistics about Nginx logs stored in MongoDB.
@@ -15,13 +16,14 @@ def log_stats(logs_collection, option=None):
     Returns:
         None
     """
-    #total_logs (int): The total number of logs in the collection.
+    # total_logs (int): The total number of logs in the collection.
     total_logs = logs_collection.count_documents({})
     print("{} logs".format(total_logs))
 
     if option:
         if option in METHODS:
-        #method_counts (dict): A dictionary containing counts of each HTTP method used.
+            # method_counts (dict): A dictionary containing counts of
+            # each HTTP method used.
             method_count = logs_collection.count_documents({"method": option})
             print("method {}: {}".format(option, method_count))
         return
@@ -30,9 +32,10 @@ def log_stats(logs_collection, option=None):
         method_count = logs_collection.count_documents({"method": method})
         print("method {}: {}".format(method, method_count))
 
-    #status_check_count (int): The count of logs with method GET and path /status.
+    # status_check_count (int): The count of logs with
+    # method GET and path /status.
     status_check_count = logs_collection.count_documents(
-            {"method": "GET", "path": "/status"})
+        {"method": "GET", "path": "/status"})
     print("{} status check".format(status_check_count))
 
 
